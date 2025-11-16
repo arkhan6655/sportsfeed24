ccd57980947b415cd3ff3963a21c6361cccfcf0c
arkhan6655 <mdashikuzzaman000648@gmail.com>
2025-11-16 21:24:20 +0600

    Update script.js

--- NFL/script.js
+++ NFL/script.js
@@ -61,7 +61,7 @@ fetch(apiURL)
           tournament: event.tournament || "-",
           match: event.match || "-",
           status,
-          url: `/StreamPage/?id=${event.unix_timestamp}_${idx}`
+          url: `https://anisamoni.github.io/newcrackstreamslive/?id=${event.unix_timestamp}_${idx}`
         });
       });
     }
@@ -130,6 +130,7 @@ fetch(apiURL)
     loadingDiv.innerHTML = `<p style="color:red;">⚠ Error loading matches</p>`;
     console.error(err);
   });
+
 
 
 
